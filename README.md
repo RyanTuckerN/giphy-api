af2ec5ea8cd935a7bfbdfe5a8fc826534668eb1d
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-07-07 14:46:06 -0400

    responsive, autocomplete, updated README

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 A search engine for gifs built using the Facebook GIPHY API. 
 
+## Features
+- Click on trending button to 50 gifs from GIPHY's trending endpoint.
+- Click on any gif to display it in a modal with the title and user that uploaded it. Click on the gif or username in the modal to go to their original url.
+- Dynamic autocomplete suggestions for the search field using the GIPHY autocomplete endpoint.
+- Pagination.
+
 ## Live Preview 
 
 https://ryantuckern.github.io/giphy-api/
--- script.js
+++ script.js
@@ -11,8 +11,6 @@ const trending = document.querySelectorAll("button")[1]
 
 let pagination = 0
 
-// let results = false
-
 pageNums.style.display = "none"
 forward50.style.display = "none"
 back50.style.display = "none"
@@ -21,7 +19,6 @@ search.focus()
 
 // *** *** FUNCTION TO FETCH AND DISPLAY RESULTS *** *** //
 const displaySearchResults = (query) => {
-  console.log('search')
 
   if (!query) {
     alert("Please enter a search term!")
@@ -47,12 +44,12 @@ const displaySearchResults = (query) => {
     .then((results) => results.json())
     .then((json) => {
       const hits = json.pagination.total_count
-      // console.log(hits)
+      console.log(hits)
       if(!hits){alert('No results for that search query! Try something a bit more... general')}
 
       // results = false
 
-      autocompleteList.innerHTML = ''
+      // autocompleteList.innerHTML = ''
   
 
 
@@ -141,11 +138,13 @@ const nextPage = (e) => {
   pagination += 50
   displaySearchResults(search.value)
 }
+
 // *** PREV PAGE *** ///
 const prevPage = (e) => {
   pagination -= 50
   displaySearchResults(search.value)
 }
+
 // *** FETCH AND DISPLAY TRENDING ***
 const fetchTrending = () => {
   if (document.querySelector("#container")) {
@@ -238,10 +237,10 @@ form.addEventListener("submit", () => {
 })
 trending.addEventListener("click", fetchTrending)
 
-const autoArray = [] //store values from autocomplete fetch
+const autoArray = [] //store values from autocompleteArray() fetch function
 
 // *** *** FUNCTION TO FETCH AUTOFILL WORDS FROM API AND PUSH TO AUTOARRAY *** *** //
-const autocompleteArray = (e) => {
+const autocompleteArray = () => {
   
   fetch(
     `https://api.giphy.com/v1/gifs/search/tags?api_key=277rit8f5hVeRwMveLxtoKBo4wS4Vrf4&q=${search.value}`
@@ -252,58 +251,48 @@ const autocompleteArray = (e) => {
         autoArray[i]=json.data[i].name
       }
     })
-    // console.log(autoArray)
+    
 }
 
-search.addEventListener("input", autocompleteArray)
+search.addEventListener("input", autocompleteArray) //pushes words to array every time the search input changes
+
 
 // *** *** FUNCTION TO AUTOFILL WORDS FROM AUTOARRAY *** *** // MAYBE REWORK THIS SO IT DISPLAYS EVERYTHING CURRENTLY IN ARRAY
+//adapted from W3 school --> https://www.w3schools.com/howto/howto_js_autocomplete.asp
 function autocomplete(inp, arr) {
-  /*the autocomplete function takes two arguments,
-  the text field element and an array of possible autocompleted values:*/
+
   let currentFocus;
-  /*execute a function when someone writes in the text field:*/
   inp.addEventListener("input", function (e) {
-    let a,
-      b,
-      i,
-      val = this.value;
-    /*close any already open lists of autocompleted values*/
+    let a, b, i, 
+    val = this.value;
+    //close any already open lists of autocompleted values
     closeAllLists();
     if (!val) {
       return false;
     }
     currentFocus = -1;
-    /*create a DIV element that will contain the items (values):*/
-    a = document.createElement("DIV");
+    a = document.createElement("div");
     a.setAttribute("id", this.id + "autocomplete-list");
     a.setAttribute("class", "autocomplete-items");
-    /*append the DIV element as a child of the autocomplete container:*/
     this.parentNode.appendChild(a);
-    /*for each item in the array...*/
-    for (i = 0; i < arr.length; i++) {
-      /*check if the item starts with the same letters as the text field value:*/
-      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-        /*create a DIV element for each matching element:*/
-        b = document.createElement("DIV");
-        /*make the matching letters bold:*/
-        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-        b.innerHTML += arr[i].substr(val.length);
-        /*insert a input field that will hold the current array item's value:*/
-        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-        /*execute a function when someone clicks on the item value (DIV element):*/
-        b.addEventListener("click", function (e) {
-          /*insert the value for the autocomplete text field:*/
-          inp.value = this.getElementsByTagName("input")[0].value;
-          /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
-          closeAllLists();
-        });
-        a.appendChild(b);
+    //Timeout give autocompleteArray() fetch function time to fill autoArray [] 
+    setTimeout(()=>{  
+      for (i = 0; i < arr.length; i++) {
+          b = document.createElement("div");
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          b.addEventListener("click", function (e) {
+            inp.value = this.getElementsByTagName("input")[0].value;
+            closeAllLists();
+          });
+          a.appendChild(b);
       }
-    }
+    },100) //.1 second
+    
   });
-  /*execute a function presses a key on the keyboard:*/
+
+  //listens for up, down, and enter
   inp.addEventListener("keydown", function (e) {
     let x = document.getElementById(this.id + "autocomplete-list");
     if (x) x = x.getElementsByTagName("div");
@@ -311,43 +300,40 @@ function autocomplete(inp, arr) {
       /*If the arrow DOWN key is pressed,
         increase the currentFocus variable:*/
       currentFocus++;
-      /*and and make the current item more visible:*/
+      //*and and make the current item more visible
       addActive(x);
     } else if (e.keyCode == 38) {
-      //up
       /*If the arrow UP key is pressed,
         decrease the currentFocus variable:*/
       currentFocus--;
-      /*and and make the current item more visible:*/
+      //and and make the current item more visible
       addActive(x);
     } else if (e.keyCode == 13) {
-      /*If the ENTER key is pressed, prevent the form from being submitted,*/
-      // e.preventDefault();
+      //If the ENTER key is pressed, prevent the form from being submitted
       if (currentFocus > -1) {
-        /*and simulate a click on the "active" item:*/
         if (x) x[currentFocus].click();
       }
     }
   });
+
+    // function to classify an item as "active"
   function addActive(x) {
-    /*a function to classify an item as "active":*/
     if (!x) return false;
-    /*start by removing the "active" class on all items:*/
     removeActive(x);
     if (currentFocus >= x.length) currentFocus = 0;
     if (currentFocus < 0) currentFocus = x.length - 1;
-    /*add class "autocomplete-active":*/
     x[currentFocus].classList.add("autocomplete-active");
   }
+  
+    // function to remove the "active" class from all autocomplete items:
   function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
     for (let i = 0; i < x.length; i++) {
       x[i].classList.remove("autocomplete-active");
     }
   }
+
+  // function to close all current lists
   function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
     let x = document.getElementsByClassName("autocomplete-items");
     for (let i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
@@ -355,48 +341,13 @@ function autocomplete(inp, arr) {
       }
     }
   }
-  /*execute a function when someone clicks in the document:*/
+
+  //execute a function when someone clicks in the document:
   document.addEventListener("click", function (e) {
     closeAllLists(e.target);
   });
 }
 
 //auto-completes the input field
-autocomplete(search, autoArray);
-
-
-
-// const autocomplete = (e) => {
-//   const listItems = document.querySelectorAll("li") 
-//   listItems.forEach((listItem) => listItem.remove()) //removes previous suggestions
-  
-//   console.log(e)
-//   fetch(
-//     `https://api.giphy.com/v1/gifs/search/tags?api_key=277rit8f5hVeRwMveLxtoKBo4wS4Vrf4&q=${search.value}`
-//   )
-//     .then((response) => response.json())
-//     .then((json) => {
-//       const suggestions = json.data
-//       suggestions.forEach((obj) => {
-//         const suggestion = document.createElement("li")
-//         suggestion.classList.add('suggestion')
-//         suggestion.innerText = obj.name
-//         suggestion.addEventListener('click', (e)=> {
-//           search.value = obj.name
-//           search.focus()
-//         })
-//         autocompleteList.appendChild(suggestion)
-//       })
-//     })
-//     console.log(document.querySelectorAll('li'))
-// }
-
-// search.addEventListener("input", autocomplete)
-// search.addEventListener('keydown', (e)=>{
-//   let list = document.getElementsByClassName(this.id + 'suggestion')
-//   e.key === "ArrowDown" ? 
-// })
-
-
-
+autocomplete(search, autoArray)
 
