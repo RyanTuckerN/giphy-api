d5215dc52c893cab2fcdeaebc343286b5b24c5c9
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-07-06 23:57:59 -0400

    autocomplete works

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const baseUrl = "https://api.giphy.com/v1/gifs/"
 const apiKey = "277rit8f5hVeRwMveLxtoKBo4wS4Vrf4"
 const search = document.querySelector("input")
+const autocompleteList = document.querySelector('.suggestions')
 const form = document.querySelector("form")
-const section = document.querySelector("section")
+const resultsSection = document.querySelector("section")
 const forward50 = document.querySelector("#right")
 const back50 = document.querySelector("#left")
 const pageNums = document.querySelector("#page-nums")
@@ -10,13 +11,18 @@ const trending = document.querySelectorAll("button")[1]
 
 let pagination = 0
 
+// let results = false
+
 pageNums.style.display = "none"
 forward50.style.display = "none"
 back50.style.display = "none"
 
-search.focus()
+search.focus() 
 
+// *** *** FUNCTION TO FETCH AND DISPLAY RESULTS *** *** //
 const displaySearchResults = (query) => {
+  console.log('search')
+
   if (!query) {
     alert("Please enter a search term!")
     return
@@ -27,7 +33,8 @@ const displaySearchResults = (query) => {
   document.querySelectorAll("img").forEach((image) => {
     image.remove()
   })
-
+  
+  
   fetch(
     baseUrl +
       "search?api_key=" +
@@ -41,6 +48,13 @@ const displaySearchResults = (query) => {
     .then((json) => {
       const hits = json.pagination.total_count
       // console.log(hits)
+      if(!hits){alert('No results for that search query! Try something a bit more... general')}
+
+      // results = false
+
+      autocompleteList.innerHTML = ''
+  
+
 
       !pagination
         ? (back50.style.display = "none")
@@ -67,7 +81,7 @@ const displaySearchResults = (query) => {
         if (ob.user !== undefined) {
           gif.setAttribute("data-user-url", ob.user.profile_url)
         }
-        section.appendChild(gif)
+        resultsSection.appendChild(gif)
       })
 
       const gifs = document.querySelectorAll("img")
@@ -104,7 +118,7 @@ const displaySearchResults = (query) => {
       <button class="btn btn-dark" id='close-modal'>Close</button>
       </div>`
 
-        section.append(newModal)
+        resultsSection.append(newModal)
 
         // *** *** *** Modal removal *** *** *** //
         const modalWrapper = document.querySelector("#modal-wrapper")
@@ -119,18 +133,20 @@ const displaySearchResults = (query) => {
         gif.addEventListener("click", addModal)
       })
     })
+    
 }
 
+// *** NEXT PAGE *** ///
 const nextPage = (e) => {
   pagination += 50
   displaySearchResults(search.value)
 }
-
+// *** PREV PAGE *** ///
 const prevPage = (e) => {
   pagination -= 50
   displaySearchResults(search.value)
 }
-
+// *** FETCH AND DISPLAY TRENDING ***
 const fetchTrending = () => {
   if (document.querySelector("#container")) {
     document.querySelector("#container").remove()
@@ -143,8 +159,6 @@ const fetchTrending = () => {
   forward50.style.display = "none"
   back50.style.display = "none"
   pageNums.style.display = "none"
-
-
 
   fetch(
     "https://api.giphy.com/v1/gifs/trending?api_key=277rit8f5hVeRwMveLxtoKBo4wS4Vrf4"
@@ -161,7 +175,7 @@ const fetchTrending = () => {
         if (ob.user !== undefined) {
           gif.setAttribute("data-user-url", ob.user.profile_url)
         }
-        section.appendChild(gif)
+        resultsSection.appendChild(gif)
       })
 
       const gifs = document.querySelectorAll("img")
@@ -198,7 +212,7 @@ const fetchTrending = () => {
       <button class="btn btn-dark" id='close-modal'>Close</button>
       </div>`
 
-        section.append(newModal)
+        resultsSection.append(newModal)
 
         // *** *** *** Modal removal *** *** *** //
         const modal = document.querySelector("#modal")
@@ -224,10 +238,165 @@ form.addEventListener("submit", () => {
 })
 trending.addEventListener("click", fetchTrending)
 
+const autoArray = [] //store values from autocomplete fetch
 
-// const autocomplete = () => {
-//   fetch('https://api.giphy.com/v1/gifs/search/tags?api_key=277rit8f5hVeRwMveLxtoKBo4wS4Vrf4&q=fo')
-//     .then(response => response.json())
-//     .then(json => console.log(json))
+// *** *** FUNCTION TO FETCH AUTOFILL WORDS FROM API AND PUSH TO AUTOARRAY *** *** //
+const autocompleteArray = (e) => {
+  
+  fetch(
+    `https://api.giphy.com/v1/gifs/search/tags?api_key=277rit8f5hVeRwMveLxtoKBo4wS4Vrf4&q=${search.value}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      for(let i = 0; i < json.data.length; i++){
+        autoArray[i]=json.data[i].name
+      }
+    })
+    // console.log(autoArray)
+}
+
+search.addEventListener("input", autocompleteArray)
+
+// *** *** FUNCTION TO AUTOFILL WORDS FROM AUTOARRAY *** *** // MAYBE REWORK THIS SO IT DISPLAYS EVERYTHING CURRENTLY IN ARRAY
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  let currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function (e) {
+    let a,
+      b,
+      i,
+      val = this.value;
+    /*close any already open lists of autocompleted values*/
+    closeAllLists();
+    if (!val) {
+      return false;
+    }
+    currentFocus = -1;
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    /*append the DIV element as a child of the autocomplete container:*/
+    this.parentNode.appendChild(a);
+    /*for each item in the array...*/
+    for (i = 0; i < arr.length; i++) {
+      /*check if the item starts with the same letters as the text field value:*/
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        /*create a DIV element for each matching element:*/
+        b = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        /*insert a input field that will hold the current array item's value:*/
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        /*execute a function when someone clicks on the item value (DIV element):*/
+        b.addEventListener("click", function (e) {
+          /*insert the value for the autocomplete text field:*/
+          inp.value = this.getElementsByTagName("input")[0].value;
+          /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+          closeAllLists();
+        });
+        a.appendChild(b);
+      }
+    }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function (e) {
+    let x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+      currentFocus++;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 38) {
+      //up
+      /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+      currentFocus--;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      /*If the ENTER key is pressed, prevent the form from being submitted,*/
+      // e.preventDefault();
+      if (currentFocus > -1) {
+        /*and simulate a click on the "active" item:*/
+        if (x) x[currentFocus].click();
+      }
+    }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = x.length - 1;
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (let i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    let x = document.getElementsByClassName("autocomplete-items");
+    for (let i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+  });
+}
+
+//auto-completes the input field
+autocomplete(search, autoArray);
+
+
+
+// const autocomplete = (e) => {
+//   const listItems = document.querySelectorAll("li") 
+//   listItems.forEach((listItem) => listItem.remove()) //removes previous suggestions
+  
+//   console.log(e)
+//   fetch(
+//     `https://api.giphy.com/v1/gifs/search/tags?api_key=277rit8f5hVeRwMveLxtoKBo4wS4Vrf4&q=${search.value}`
+//   )
+//     .then((response) => response.json())
+//     .then((json) => {
+//       const suggestions = json.data
+//       suggestions.forEach((obj) => {
+//         const suggestion = document.createElement("li")
+//         suggestion.classList.add('suggestion')
+//         suggestion.innerText = obj.name
+//         suggestion.addEventListener('click', (e)=> {
+//           search.value = obj.name
+//           search.focus()
+//         })
+//         autocompleteList.appendChild(suggestion)
+//       })
+//     })
+//     console.log(document.querySelectorAll('li'))
 // }
-// autocomplete()
+
+// search.addEventListener("input", autocomplete)
+// search.addEventListener('keydown', (e)=>{
+//   let list = document.getElementsByClassName(this.id + 'suggestion')
+//   e.key === "ArrowDown" ? 
+// })
+
+
+
+
